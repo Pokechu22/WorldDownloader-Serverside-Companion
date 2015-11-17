1e4c13ff9ef9a0101b583f243fc28ea20bbb5dc5
Pokechu22 <Pokechu022@gmail.com>
2015-11-16 18:27:52 -0800

    Stop listening to IslandRestartEvent.
    It is actually fired _after_ IslandNewEvent.  So we can simply make
    IslandNewEvent overwrite the old permissions instead.

--- Extensions/ASkyBlock/src/main/java/wdl/askyblock/ASkyBlockRangeProducer.java
+++ Extensions/ASkyBlock/src/main/java/wdl/askyblock/ASkyBlockRangeProducer.java
@@ -15,7 +15,6 @@ import com.wasteofplastic.askyblock.Settings;
 import com.wasteofplastic.askyblock.events.IslandJoinEvent;
 import com.wasteofplastic.askyblock.events.IslandLeaveEvent;
 import com.wasteofplastic.askyblock.events.IslandNewEvent;
-import com.wasteofplastic.askyblock.events.IslandResetEvent;
 
 import wdl.range.IRangeGroup;
 import wdl.range.IRangeProducer;
@@ -87,23 +86,12 @@ public class ASkyBlockRangeProducer implements IRangeProducer, Listener {
 	 */
 	@EventHandler
 	public void onNewIsland(IslandNewEvent e) {
+		Player player = e.getPlayer();
+		
 		ProtectionRange range = getProtectionRangeForIsland(
 				e.getIslandLocation(), e.getProtectionSize());
 		
-		group.addRanges(e.getPlayer(), range);
-	}
-	
-	/**
-	 * Occurs when ASkyBlock fires an {@link IslandResetEvent}: a player has
-	 * reset their island.
-	 */
-	@EventHandler
-	public void onResetIsland(IslandResetEvent e) {
-		Player player = e.getPlayer();
-		
-		String tag = player.getName() + "'s island";
-		
-		group.removeRangesByTags(player, tag);
+		group.setTagRanges(player, player.getName() + "'s island", range);
 	}
 	
 	/**
