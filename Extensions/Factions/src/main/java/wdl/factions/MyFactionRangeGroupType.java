962bf01c11da4405e0532c6b8f01440c0bbe6b1d
Pokechu22 <Pokechu022@gmail.com>
2015-12-20 15:02:02 -0800

    Initial version of the factions permission plugin.
    Right now, I have it give ranges for the faction the player owns or is a
    member of, if they have the right permission.  The next thing I'll do is
    have it show nearby factions.

---
+++ Extensions/Factions/src/main/java/wdl/factions/MyFactionRangeGroupType.java
@@ -0,0 +1,32 @@
+package wdl.factions;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+
+import wdl.range.IRangeGroup;
+import wdl.range.IRangeGroupType;
+
+public class MyFactionRangeGroupType implements IRangeGroupType<MyFactionRangeProducer> {
+	private final FactionsSupportPlugin plugin;
+	
+	public MyFactionRangeGroupType(FactionsSupportPlugin plugin) {
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public MyFactionRangeProducer createRangeProducer(IRangeGroup group,
+			ConfigurationSection config) {
+		MyFactionRangeProducer producer = new MyFactionRangeProducer(group,
+				plugin);
+		Bukkit.getPluginManager().registerEvents(producer, plugin);
+		return producer;
+	}
+	
+	@Override
+	public boolean isValidConfig(ConfigurationSection config,
+			List<String> warnings, List<String> errors) {
+		return true;
+	}
+}
