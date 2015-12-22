bb7412a13ce771df8d7bf2222ab7e961af4b5206
Pokechu22 <Pokechu022@gmail.com>
2015-12-22 15:39:52 -0800

    Attempt to compact multiple ranges into one.
    This probably is going to go horibly wrong, but it's a start.

--- src/main/java/wdl/RangeGroup.java
+++ src/main/java/wdl/RangeGroup.java
@@ -1,7 +1,11 @@
 package wdl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.entity.Player;
 
@@ -9,6 +13,9 @@ import wdl.range.IRangeGroup;
 import wdl.range.IRangeGroupType;
 import wdl.range.IRangeProducer;
 import wdl.range.ProtectionRange;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
 /**
  * Group of {@link ProtectionRange}s, which can be edited and will send
@@ -64,9 +71,8 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(player,
-				WDLPackets.createWDLPacket5(groupName, false,
-						Arrays.asList(ranges)));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket5(groupName,
+				false, compactRanges(ranges)));
 	}
 	
 	@Override
@@ -86,8 +92,8 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(player,
-				WDLPackets.createWDLPacket5(groupName, false, ranges));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket5(groupName,
+				false, compactRanges(ranges)));
 	}
 
 	@Override
@@ -107,9 +113,8 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(player,
-				WDLPackets.createWDLPacket5(groupName, true,
-						Arrays.asList(ranges)));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket5(groupName, true,
+				compactRanges(ranges)));
 	}
 	
 	@Override
@@ -129,8 +134,8 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(player,
-				WDLPackets.createWDLPacket5(groupName, true, ranges));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket5(groupName, true,
+				compactRanges(ranges)));
 	}
 
 	@Override
@@ -197,10 +202,8 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(
-				player,
-				WDLPackets.createWDLPacket7(groupName, tag,
-						Arrays.asList(ranges)));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket7(groupName, tag,
+				compactRanges(ranges)));
 	}
 	
 	@Override
@@ -226,14 +229,243 @@ class RangeGroup implements IRangeGroup {
 			}
 		}
 		
-		plugin.queuePacket(player,
-				WDLPackets.createWDLPacket7(groupName, tag, ranges));
+		plugin.queuePacket(player, WDLPackets.createWDLPacket7(groupName, tag,
+				compactRanges(ranges)));
 	}
 
 	@Override
 	public boolean isWDLPlayer(Player player) {
 		return player.getListeningPluginChannels().contains(
 				WDLCompanion.CONTROL_CHANNEL_NAME);
+	}
+	
+	/**
+	 * Convert the given list of ranges into an equivalent list with ranges
+	 * partially merged together.
+	 */
+	private List<ProtectionRange> compactRanges(ProtectionRange... ranges) {
+		return compactRanges(Arrays.asList(ranges));
+	}
+	
+	/**
+	 * Convert the given list of ranges into an equivalent list with ranges
+	 * partially merged together.
+	 */
+	private List<ProtectionRange> compactRanges(List<ProtectionRange> ranges) {
+		// Based off of http://stackoverflow.com/a/8072813/3991344
+		// Try to form horizontal or vertical rectangles.  Not perfect, but
+		// hopefully it's good enough in practice.
+		
+		/**
+		 * A single point, for use in calculations.
+		 */
+		class Point {
+			public Point(int x, int z) {
+				this.x = x;
+				this.z = z;
+			}
+			public Point(ProtectionRange range) {
+				assert range.x1 == range.x2;
+				assert range.z1 == range.z2;
+				
+				this.x = range.x1;
+				this.z = range.z1;
+			}
+			
+			public final int x;
+			public final int z;
+			
+			public Point oneLeft() {
+				return new Point(this.x + 1, this.z);
+			}
+			public Point oneDown() {
+				return new Point(this.x, this.z + 1);
+			}
+			
+			@Override
+			public int hashCode() {
+				final int prime = 1117; //Larger prime because x and z may be small.
+				int result = 1;
+				result = prime * result + x;
+				result = prime * result + z;
+				return result;
+			}
+			
+			@Override
+			public boolean equals(Object obj) {
+				if (this == obj) {
+					return true;
+				}
+				if (obj == null) {
+					return false;
+				}
+				if (!(obj instanceof Point)) {
+					return false;
+				}
+				Point other = (Point) obj;
+				if (x != other.x) {
+					return false;
+				}
+				if (z != other.z) {
+					return false;
+				}
+				return true;
+			}
+			
+			@Override
+			public String toString() {
+				return "Point [x=" + x + ", z=" + z + "]";
+			}
+		}
+		
+		Multimap<String, ProtectionRange> rangesByTag = HashMultimap.create();
+		
+		for (ProtectionRange range : ranges) {
+			rangesByTag.put(range.tag, range);
+		}
+		
+		List<ProtectionRange> finalRanges = new ArrayList<>();
+		
+		for (Map.Entry<String, Collection<ProtectionRange>> e : rangesByTag.asMap().entrySet()) {
+			String tag = e.getKey();
+			
+			List<ProtectionRange> newRanges = new ArrayList<>();
+			
+			int lowestX = Integer.MAX_VALUE;
+			int lowestZ = Integer.MAX_VALUE;
+			int highestX = Integer.MIN_VALUE;
+			int highestZ = Integer.MIN_VALUE;
+			
+			Map<Point, ProtectionRange> rangesByLocation = new HashMap<>();
+			
+			for (ProtectionRange range : e.getValue()) {
+				if (range.x1 == range.x2 && range.z1 == range.z2) {
+					if (range.x1 < lowestX) {
+						lowestX = range.x1;
+					}
+					if (range.z1 < lowestZ) {
+						lowestZ = range.z1;
+					}
+					if (range.x1 > highestX) {
+						highestX = range.x1;
+					}
+					if (range.z1 > highestZ) {
+						highestZ = range.z1;
+					}
+					// Will also overwrite other ranges if there's two at the
+					// same place, which is beneficial.
+					rangesByLocation.put(new Point(range), range);
+				} else {
+					newRanges.add(range);
+				}
+			}
+			
+			int rectStartX = 0, rectStartZ = 0;
+			int rectEndX = 0, rectEndZ = 0;
+			boolean inRectangle = false;
+			
+			// Horizontal groups first.
+			for (int z = lowestZ; z < highestZ; z++) {
+				for (int x = lowestX; x < highestX; x++) {
+					Point point = new Point(x, z);
+					
+					if (rangesByLocation.containsKey(point)) {
+						if (inRectangle) {
+							//Expand the rectangle.
+							rectEndX = x;
+							
+							//We've used up this point.
+							rangesByLocation.remove(point);
+						} else {
+							// Start a new rectangle if there
+							// is at least one more range.
+							if (rangesByLocation.containsKey(point.oneLeft())) {
+								inRectangle = true;
+								rectStartX = x;
+								rectStartZ = z;
+								rectEndX = x;
+								rectEndZ = z;
+								
+								//We've used up this point.
+								rangesByLocation.remove(point);
+							}
+						}
+					} else {
+						if (inRectangle) {
+							//Current rectangle finished - edge reached.
+							ProtectionRange range = new ProtectionRange(tag, rectStartX,
+									rectStartZ, rectEndX, rectEndZ);
+							newRanges.add(range);
+							inRectangle = false;
+						}
+					}
+				}
+				
+				if (inRectangle) {
+					//Current rectangle finished - line finished.
+					ProtectionRange range = new ProtectionRange(tag, rectStartX,
+							rectStartZ, rectEndX, rectEndZ);
+					newRanges.add(range);
+					inRectangle = false;
+				}
+			}
+			
+			// Now do vertical groups.
+			for (int x = lowestX; x < highestX; x++) {
+				for (int z = lowestZ; z < highestZ; z++) {
+					Point point = new Point(x, z);
+					
+					if (rangesByLocation.containsKey(point)) {
+						if (inRectangle) {
+							//Expand the rectangle.
+							rectEndZ = z;
+							
+							//We've used up this point.
+							rangesByLocation.remove(point);
+						} else {
+							// Start a new rectangle if there
+							// is at least one more range.
+							if (rangesByLocation.containsKey(point.oneDown())) {
+								inRectangle = true;
+								rectStartX = x;
+								rectStartZ = z;
+								rectEndX = x;
+								rectEndZ = z;
+								
+								//We've used up this point.
+								rangesByLocation.remove(point);
+							}
+						}
+					} else {
+						if (inRectangle) {
+							//Current rectangle finished - edge reached.
+							ProtectionRange range = new ProtectionRange(tag, rectStartX,
+									rectStartZ, rectEndX, rectEndZ);
+							newRanges.add(range);
+							inRectangle = false;
+						}
+					}
+				}
+				
+				if (inRectangle) {
+					//Current rectangle finished - line finished.
+					ProtectionRange range = new ProtectionRange(tag, rectStartX,
+							rectStartZ, rectEndX, rectEndZ);
+					newRanges.add(range);
+					inRectangle = false;
+				}
+			}
+			
+			finalRanges.addAll(newRanges);
+		}
+		
+		
+		// TODO: Temporary logging.
+		plugin.getLogger().info(
+				"Compacted " + ranges.size() + " ranges into "
+						+ finalRanges.size());
+		
+		return finalRanges;
 	}
 	
 	/**
